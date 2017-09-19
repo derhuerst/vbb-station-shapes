bab623643ed46335159176ed6b3379bcc896e2af
Jannis R <mail@jannisr.de>
2017-09-19 14:44:36 +0200

    WIP

--- build/extract-shapes.js
+++ build/extract-shapes.js
@@ -6,25 +6,30 @@ const {SVGGraphics} = require('pdfjs-dist/lib/display/svg')
 const extractShapes = (page, filterShapes) => {
 	const shapes = []
 
-	const check = (ctx, group) => {
+	const check = (ctx, group, svg) => {
 		if (filterShapes(ctx)) {
 			const shape = ctx.element
-			shapes.push([shape.attributes, group.attributes])
+			shapes.push([shape.attributes, group.attributes, svg.attributes])
 		}
 	}
 
+	// todo: this.current.textMatrix
+	// todo: this.current.fontMatrix
+	// todo: this.current.x, this.current.y
+	// todo: this.current.lineX, this.current.lineY
+	// todo: this.current.lineWidth
+	// todo: this.current.dependencies
+	// todo: this.svg
+	// todo: this.current.clipGroup.attributes['clip-group']
+	// todo: this.current.activeClipUrl
 	class Renderer extends SVGGraphics {
 		stroke () {
-			check(this.current, this.tgrp)
+			check(this.current, this.tgrp, this.svg)
 			return super.stroke()
 		}
 		fill () {
-			check(this.current, this.tgrp)
+			check(this.current, this.tgrp, this.svg)
 			return super.fill()
-		}
-		eoFill () {
-			check(this.current, this.tgrp)
-			return super.eoFill()
 		}
 	}
 
--- build/index.js
+++ build/index.js
@@ -15,28 +15,32 @@ const data = fs.readFileSync(path.join(__dirname, '9014101.pdf'))
 
 // const HOUSE = 'rgb(133,134,138)'
 // const BUS_STOP = 'rgb(158,42,150)'
-// const SUBWAY_LINE = 'rgb(0,110,184)'
+const SUBWAY_LINE = 'rgb(0,110,184)'
 
 const ENTRY = 'rgb(170,205,234)'
 const STATION = 'rgb(88,158,210)'
 const filterShapes = (ctx) => {
-	return ctx.strokeColor === ENTRY || ctx.strokeColor === STATION // todo: parse
+	// todo: parse
+	return ctx.strokeColor === ENTRY
+	|| ctx.strokeColor === STATION
+	|| ctx.strokeColor === SUBWAY_LINE
 }
 
 // alternative: https://github.com/TrySound/postcss-value-parser
-const normalizeShape = (ctx, group) => {
+const normalizeShape = (ctx, group, svg) => {
 	if (!ctx.d) throw new Error('failed to normalize shape, missing d attribute')
 
 	const p = createPath(ctx.d)
+	if (svg.transform) p.transform(svg.transform)
 	if (group.transform) p.transform(group.transform)
 	if (ctx.transform) p.transform(ctx.transform)
 
 	return p.abs().round(.3).toString()
 }
 
-const createSVGPath = ([ctx, group]) => {
+const createSVGPath = ([ctx, group, svg]) => {
 	const a = Object.assign({}, omit(group, ['transform']), omit(ctx, ['transform']))
-	a.d = normalizeShape(ctx, group)
+	a.d = normalizeShape(ctx, group, svg)
 
 	return h('path', a)
 }
@@ -46,11 +50,12 @@ pdf.getDocument(data)
 .then((page) => extractShapes(page, filterShapes))
 .then((shapes) => {
 	const svg = `\
-	<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
+	<svg width="500" height="500" viewBox="0 0 2000 2000" xmlns="http://www.w3.org/2000/svg">
 		${shapes.map(createSVGPath).map(toString).join('\n')}
 	</svg>`
 	fs.writeFileSync(path.join(__dirname, '../foo.svg'), svg)
 })
 .catch((err) => {
 	console.error(err)
+	process.exit(1)
 })
