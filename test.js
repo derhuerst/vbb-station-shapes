bd25c7774d54a0036b10466493bc85623cdedd2d
Jannis R <mail@jannisr.de>
2017-10-28 19:50:29 +0200

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -2,7 +2,26 @@
 
 const test = require('tape')
 
-const todo = require('.')
+const pathForShape = require('.')
 
-test('todo', (t) => {
+const ruhleben = '900000025202'
+const doesNotExist = '123456789012'
+
+test('should throw if not used properly', (t) => {
+	t.plan(2)
+	t.throws(() => pathForShape())
+	t.throws(() => pathForShape(''))
+})
+
+test('should return null if shape does not exist', (t) => {
+	t.plan(1)
+	t.equal(pathForShape(doesNotExist), null)
+})
+
+test('should return a valid GeoJSON shape file', (t) => {
+	t.plan(1)
+	const path = pathForShape(doesNotExist)
+	const shape = require(path)
+	t.ok(shape)
+	// todo: test if valid GeoJSON
 })
