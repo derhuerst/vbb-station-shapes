ef8ade56306ba5272912e3b9f7598261c785d5bb
Jannis R <mail@jannisr.de>
2017-10-31 14:22:29 +0100

    update alkis-berlin-client

--- build/shapes-stream.js
+++ build/shapes-stream.js
@@ -1,24 +1,27 @@
 'use strict'
 
 const {getItems} = require('alkis-berlin-client')
-const parseStructure = require('alkis-berlin-client/lib/parse-structure')
 const {findIn, textOf} = require('alkis-berlin-client/lib/helpers')
 const filter = require('stream-filter')
 
 const catCodes = require('./cat-codes')
 
-const parse = (s) => {
-	const r = parseStructure(s)
-	r.railwayCat = {
+const parseRailwayCat = (s) => {
+	return {
 		id: textOf(findIn(s, 'fis:Bahnkategorie_schluessel')) || null,
 		name: textOf(findIn(s, 'fis:Bahnkategorie_bezeichnung')) || null
 	}
-	r.trainStationCat = {
+}
+const parseTrainStationCat = (s) => {
+	return {
 		id: textOf(findIn(s, 'fis:Bahnhofskategorie_schluessel')) || null,
 		// note the typo!
 		name: textOf(findIn(s, 'fis:Bahnhofskategorie_bezeichnug')) || null
 	}
-	return r
+}
+const additionalFields = {
+	railwayCat: parseRailwayCat,
+	trainStationCat: parseTrainStationCat
 }
 
 const haltestelle = '1020'
@@ -35,7 +38,7 @@ const createShapesStream = (bbox) => {
 	if (west <= east) throw new Error('west must be larger than east.')
 	if (north <= south) throw new Error('north must be larger than south.')
 
-	const src = getItems(layer, bbox, {parse})
+	const src = getItems(layer, bbox, {fields: additionalFields})
 	const dest = filter.obj(isValidStation)
 
 	src.pipe(dest)
