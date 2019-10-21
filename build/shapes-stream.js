3d98596b02e5b7c302925feba9cd4ad3949cb2e3
Jannis R <mail@jannisr.de>
2019-10-21 22:42:23 +0200

    pathways WIP

--- build/shapes-stream.js
+++ build/shapes-stream.js
@@ -1,52 +1,65 @@
 'use strict'
 
+const pump = require('pump')
 const {getItems} = require('alkis-berlin-client')
 const {findIn, textOf} = require('alkis-berlin-client/lib/helpers')
-const filter = require('stream-filter')
+const through = require('through2')
 
-const catCodes = require('./cat-codes')
+const layer = 'fis:s_wfs_alkis_bauwerkeflaechen'
 
+const parseBuildingCat = (s) => {
+	// todo: is there an alternative name?
+	return textOf(findIn(s, 'fis:BWF')) || null
+}
+const parseFootpathCat = (s) => {
+	// todo: is there an alternative name?
+	return textOf(findIn(s, 'fis:ART')) || null
+}
 const parseRailwayCat = (s) => {
-	return {
-		id: textOf(findIn(s, 'fis:Bahnkategorie_schluessel') || findIn(s, 'fis:BKT')) || null,
-		name: textOf(findIn(s, 'fis:Bahnkategorie_bezeichnung') || findIn(s, 'fis:BEZBKT')) || null
-	}
+	return textOf(findIn(s, 'fis:Bahnkategorie_schluessel') || findIn(s, 'fis:BKT')) || null
 }
 const parseTrainStationCat = (s) => {
-	return {
-		id: textOf(findIn(s, 'fis:Bahnhofskategorie_schluessel') || findIn(s, 'fis:BFK')) || null,
-		// note the typo!
-		name: textOf(findIn(s, 'fis:Bahnhofskategorie_bezeichnug') || findIn(s, 'fis:BEZBFK')) || null
-	}
+	return textOf(findIn(s, 'fis:Bahnhofskategorie_schluessel') || findIn(s, 'fis:BFK')) || null
 }
 const additionalFields = {
+	buildingCat: parseBuildingCat,
+	footpathCat: parseFootpathCat,
 	railwayCat: parseRailwayCat,
 	trainStationCat: parseTrainStationCat
 }
 
-const haltestelle = '1020'
-const bahnhof = '1010'
-const isValidStation = (s) => (
-	s.aaa === 'AX_Bahnverkehrsanlage' &&
-	(s.trainStationCat.id === haltestelle || s.trainStationCat.id === bahnhof) &&
-	(s.railwayCat.id in catCodes)
-)
+const parseType = ({aaa, trainStationCat, buildingCat, footpathCat}) => {
+	if (aaa === 'AX_Bahnverkehrsanlage') {
+		if (trainStationCat === '1010') return 'station'
+		if (trainStationCat === '1020') return 'stop'
+	}
+	if (aaa === 'AX_BauwerkImVerkehrsbereich' && buildingCat === '1870') {
+		return 'tunnel'
+	}
+	if (aaa === 'AX_SonstigesBauwerkOderSonstigeEinrichtung') {
+		if (buildingCat === '1610') return 'roof'
+		if (buildingCat === '1620') return 'stairs'
+	}
+	if (aaa === 'AX_WegPfadSteig' && footpathCat === '1103') {
+		return 'pathway'
+	}
+	return null
+}
 
-const layer = 'fis:s_wfs_alkis_bauwerkeflaechen'
 const createShapesStream = (bbox) => {
 	const [west, south, east, north] = bbox
 	if (west <= east) throw new Error('west must be larger than east.')
 	if (north <= south) throw new Error('north must be larger than south.')
 
-	const src = getItems(layer, bbox, {fields: additionalFields})
-	const dest = filter.obj(isValidStation)
-
-	src.pipe(dest)
-	src.on('error', (err) => {
-		dest.emit('error', err)
-	})
-
-	return dest
+	return pump(
+		getItems(layer, bbox, {fields: additionalFields}),
+		through.obj((result, _, cb) => {
+			cb(null, {...result, type: parseType(result)})
+		}),
+		(err) => {
+			if (err) console.error(err)
+		}
+	)
 }
 
 module.exports = createShapesStream
